68ec6d72f7ed9130667062a302657c3e91b2fc41
ilkyazar <ilkyaz.arabaci@gmail.com>
2020-05-21 18:39:15 +0300

    add cap

---
+++ Hw3.java
@@ -0,0 +1,39 @@
+import java.io.IOException;
+import java.util.StringTokenizer;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.Mapper;
+import org.apache.hadoop.mapreduce.Reducer;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+
+public class Hw3 {
+
+    public static void main(String[] args) throws Exception {
+        Configuration conf = new Configuration();
+
+        Job job = Job.getInstance(conf, "main");
+
+        job.setJarByClass(Hw3.class);
+
+        // input will be as:
+        // <STUDENT_ID> <COURSE_ID> <GRADE>
+        if (args[0].equals("cap")) {
+            job.setMapperClass(StudentCount.TokenizerMapper.class);
+            job.setCombinerClass(StudentCount.IntSumReducer.class);
+            job.setReducerClass(StudentCount.IntSumReducer.class);
+            job.setOutputKeyClass(Text.class);
+            job.setOutputValueClass(IntWritable.class);            
+        }
+
+        // output jar file will be tested as: hadoop jar Hw3.jar Hw3 cap input output_c
+        FileInputFormat.addInputPath(job, new Path(args[1]));
+        FileOutputFormat.setOutputPath(job, new Path(args[2]));
+
+        System.exit(job.waitForCompletion(true) ? 0 : 1);
+      }
+}
---
+++ StudentCount.java
@@ -0,0 +1,56 @@
+import java.io.IOException;
+import java.util.StringTokenizer;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.Mapper;
+import org.apache.hadoop.mapreduce.Reducer;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+
+public class StudentCount {
+
+    public static class TokenizerMapper
+       extends Mapper<Object, Text, Text, IntWritable>{
+
+    private final static IntWritable one = new IntWritable(1);
+    private Text word = new Text();
+
+    public void map(Object key, Text value, Context context
+                    ) throws IOException, InterruptedException {
+      StringTokenizer itr = new StringTokenizer(value.toString());
+      
+      int index = 0;
+
+      while (itr.hasMoreTokens()) {
+        word.set(itr.nextToken());
+
+        if (index%3 == 1) {
+          System.out.println("\nindex = " + index + "\n");
+          context.write(word, one);
+        }
+        index++;
+      }
+    }
+  }
+
+  public static class IntSumReducer
+       extends Reducer<Text,IntWritable,Text,IntWritable> {
+    private IntWritable result = new IntWritable();
+
+    public void reduce(Text key, Iterable<IntWritable> values,
+                       Context context
+                       ) throws IOException, InterruptedException {
+      int sum = 0;
+      for (IntWritable val : values) {
+        sum += val.get();
+      }
+      result.set(sum);
+      context.write(key, result);
+    }
+  }
+        
+}
